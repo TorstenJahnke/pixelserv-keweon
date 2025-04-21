e0750eaac3e815a39046c7962f4f955393fc1be6
Torsten Jahnke <torsten.jahnke@keweon.de>
2025-04-21 11:38:10 +0200

    Update util.h

--- util.h
+++ util.h
@@ -23,7 +23,7 @@
 #include <openssl/ssl.h>
 
 // preprocessor defines
-#define VERSION "2.4"
+#define VERSION "3.0.15"
 
 #define BACKLOG SOMAXCONN       // how many pending connections queue will hold
 #define DEFAULT_IP "*"          // default IP address ALL - use this in messages only
